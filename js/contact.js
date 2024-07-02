3de8309cfdd39509337ca7ca366e2d833e18b3da
halwan wijaya <wanjaya700@gmail.com>
2024-07-02 09:45:11 +0700

    Menambahkan halaman contact, dan membuat fungsi untuk mangarah langsung ke wa dan gmail

---
+++ js/contact.js
@@ -0,0 +1,6 @@
+function toWhatsapp() {
+    window.open('https://wa.me/6285719612449?text=hallo','_blank');
+}
+function toGmail() {
+    window.open('https://mail.google.com/mail/?view=cm&fs=1&to=halwanwijaya1@gmail.com&su=Hello&body=How%20are%20you','_blank');
+}
